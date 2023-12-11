76994f1fe91341f109aff179e0f16a9a8915ecd2
luweijun <214245088@qq.com>
2023-12-11 14:35:21 +0800

    fix: 优化

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,7 +25,7 @@ router.get('/api/db-check', async (ctx, next) => {
   ctx.body = {
     error: 0,
     data: {
-      name: 'lwj123',
+      name: 'lwj1234',
       ENV,
       version: packageInfo.version,
       mysqlConn: !!mysqlRes.length,
